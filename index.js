acdbf4833195dec13c972746a34f3ed51b357cb5
nishantms <>
2022-05-16 15:15:39 -0400

    sample npm publish

---
+++ index.js
@@ -0,0 +1,7 @@
+const addFns = {
+    add : function addTwoNums( num1, num2 ) {
+      return (num1 + num2) ;
+    }
+  }
+    
+  module.exports = addFns
---
+++ use-package/app.js
@@ -0,0 +1,4 @@
+const addFns = require('addition-two-numbers-module');
+
+const c= addFns.add(1,2);
+console.log(c);
